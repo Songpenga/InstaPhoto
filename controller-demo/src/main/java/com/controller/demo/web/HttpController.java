311e35febed1d91d522e23a8d20764e450a7a890
Songpenga <iiuuyy6654@gmail.com>
2023-05-11 23:50:38 +0900

    Controller Studying

---
+++ controller-demo/src/main/java/com/controller/demo/web/HttpController.java
@@ -0,0 +1,33 @@
+package com.controller.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller //File을 응답하는 컨트롤러(클라이언트가 브라우저면 .html을)
+@RestController//data를 응답하는 컨트롤러(클라이언트가 핸드폰이면 data)
+public class HttpController {
+
+	@GetMapping("/get")
+	public String get() {
+		return "<h1>get요청됨</h1>";
+	}
+	
+	@PostMapping("/post")
+	public String post() {
+		return "Post요청됨";
+	}
+	
+	@PutMapping("/put")
+	public String put() {
+		return "Put요청됨";
+	}
+	
+	@DeleteMapping("/delete")
+	public String delete() {
+		return "Delete요청됨";
+	}
+}
