8839d5031d0e03f009c2a39269acfbf00197c038
Songpenga <iiuuyy6654@gmail.com>
2023-06-01 23:57:57 +0900

    인기페이지 구현

--- photogram/src/main/java/com/cos/photogramstart/service/ImageService.java
+++ photogram/src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -3,6 +3,7 @@ package com.cos.photogramstart.service;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -22,6 +23,12 @@ import lombok.RequiredArgsConstructor;
 @Service 
 public class ImageService {
 	private final ImageRepository imageRepository;
+	
+	@Transactional(readOnly = true)
+	public List<Image> 인기사진() {
+		
+		return imageRepository.mPopular();
+	}
 	
 	@Transactional(readOnly = true) // 영속성 컨텍스트 변경 감지를 해서, 더티체킹, flush(반영) X
 	public Page<Image> 이미지스토리(int principalId, Pageable pageable){
@@ -65,4 +72,5 @@ public class ImageService {
 		
 		//System.out.println(imageEntity.toString());
 	}
+
 }
