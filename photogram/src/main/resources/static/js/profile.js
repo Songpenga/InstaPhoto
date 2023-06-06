66bca8d2d3fb0062657f57f2a3a0730098301d3a
Songpenga <iiuuyy6654@gmail.com>
2023-06-06 22:23:04 +0900

    프로필 페이지 유저 사진 변경  / 73

--- photogram/src/main/resources/static/js/profile.js
+++ photogram/src/main/resources/static/js/profile.js
@@ -83,7 +83,16 @@ function getSubscribeModalItem(u) {
 }
 
 // (3) 유저 프로파일 사진 변경 (완)
-function profileImageUpload() {
+function profileImageUpload(pageUserId, principalId) {
+	
+	console.log("pageUserId", pageUserId);
+	console.log("principalId", principalId);
+	
+	if(pageUserId != principalId){
+		alert("프로필 사진을 수정할 수 없는 유저입니다.");
+		return;
+	}
+	
 	$("#userProfileImageInput").click();
 
 	$("#userProfileImageInput").on("change", (e) => {
@@ -94,12 +103,30 @@ function profileImageUpload() {
 			return;
 		}
 
+		//서버에 이미지를 전송 73
+		let ProfileImageForm = $("#userProfileImageForm");
+		
+		//formData 객체를 이용하면 form테그의 필드와 그 값을 나타내는 일련의 key/value 쌍을 담을 수 있다.
+		let formData = new FormData(ProfileImageForm);
+		
+		$.ajax({
+			type: "put",
+			url:`/api/user/${principalId}/ProfileImageUrl`,
+			data: formData,
+			contentType: false, //필수 : x-www-form-urlencoded로 파싱되는 것을 방지
+			processData: false,	// 필수 : contentType을 false로 줬을때 QueryString로 자동 설정됨.
+			enctype: "multipart/"
+		}).done(res=>{
 		// 사진 전송 성공시 이미지 변경
-		let reader = new FileReader();
-		reader.onload = (e) => {
+			let reader = new FileReader();
+			reader.onload = (e) => {
 			$("#userProfileImage").attr("src", e.target.result);
-		}
-		reader.readAsDataURL(f); // 이 코드 실행시 reader.onload 실행됨.
+			}
+			reader.readAsDataURL(f); // 이 코드 실행시 reader.onload 실행됨
+		}).fail(error=>{
+			console.log("오류", error);
+		});
+		
 	});
 }
 
