af95eb0917fcfe43f768b4818d9e71e15389ba22
Songpenga <iiuuyy6654@gmail.com>
2023-06-10 23:52:59 +0900

    OAuth2 페이스북 로그인 진행중... - 앱정보 가져오기

--- photogram/src/main/java/com/cos/photogramstart/config/SecurityConfig.java
+++ photogram/src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity // 해당 파일로 시큐리티를 활성화
 @Configuration // IoC     
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
-
+	
 	@Bean
 	public BCryptPasswordEncoder encode() {
 		return new BCryptPasswordEncoder();
@@ -21,13 +21,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		// super 삭제 - 기존 시큐리티가 가지고 있는 기능이 다 비활성화됨.
 		http.csrf().disable();
 		http.authorizeRequests()
-			.antMatchers("/", "/user/**", "/image/**", "/subscribe/**", "/comment/**","/api/**").authenticated()
+			.antMatchers("/", "/user/**", "/image/**", "/subscribe/**", "/comment/**", "/api/**").authenticated()
 			.anyRequest().permitAll()
 			.and()
 			.formLogin()
 			.loginPage("/auth/signin") // GET
 			.loginProcessingUrl("/auth/signin") // POST -> 스프링 시큐리티가 로그인 프로세스 진행
-			.defaultSuccessUrl("/");
+			.defaultSuccessUrl("/")
+			.and()
+			.oauth2Login() // form로그인도 하는데, oauth2로그인도 할꺼야!!
+			.userInfoEndpoint() // oauth2로그인을 하면 최종응답을 회원정보를 바로 받을 수 있다.
+			.userService(null);
 	}
 	
 
